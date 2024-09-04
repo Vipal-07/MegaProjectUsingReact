408f07e6e985667d8f60371f08a68ea94b51ea13
Vikas Maurya <vm6431135@gmail.com>
2024-09-04 13:01:24 +0530

    CAt

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import {Container, Logo, LogoutBtn} from '../index'
 import { Link } from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status)
